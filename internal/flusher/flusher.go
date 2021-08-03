c1d9cd699e8319b447e6c8c03b075033c78aafe7
Oleg <sheludykov9558@gmail.com>
2021-08-03 09:15:28 +0300

    - fix remarks

--- internal/flusher/flusher.go
+++ internal/flusher/flusher.go
@@ -3,6 +3,7 @@ package flusher
 import (
 	"github.com/ozoncp/ocp-timeline-api/internal/models"
 	"github.com/ozoncp/ocp-timeline-api/internal/repo"
+	"github.com/ozoncp/ocp-timeline-api/internal/utils"
 )
 
 type Flusher interface {
@@ -23,36 +24,19 @@ type flusher struct {
 
 func (f *flusher) Flush(entities []models.Timeline) []models.Timeline {
 
-	f.chunkOperation(entities, func(chunk []models.Timeline) {
-		err := f.entityRepo.AddEntities(chunk)
+	if len(entities) == 0 {
+		return entities
+	}
+
+	chunks := utils.ChunkTimeline(entities, f.chunkSize)
+
+	for i := range chunks {
+		err := f.entityRepo.AddEntities(chunks[i])
+
 		if err != nil {
 			panic("repo not work")
 		}
-	})
+	}
 
 	return entities
-}
-
-func (f *flusher) chunkOperation(entities []models.Timeline, operationOnChunk func([]models.Timeline)) {
-	length := len(entities)
-
-	if length == 0 {
-		return
-	}
-
-	if length <= f.chunkSize {
-		operationOnChunk(entities)
-
-		return
-	}
-
-	quotient, reminder := length/f.chunkSize, length%f.chunkSize
-
-	for i := 0; i < quotient; i++ {
-		operationOnChunk(entities[i*f.chunkSize : i*f.chunkSize+f.chunkSize])
-	}
-
-	if reminder != 0 {
-		operationOnChunk(entities[quotient*f.chunkSize:])
-	}
 }
