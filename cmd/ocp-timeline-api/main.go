ed0a831e690020efef612ab12ca8f8719bd3d421
oleg <sheludykov9559@gmail.com>
2021-09-02 09:05:40 +0300

    - add proxy rest

--- cmd/ocp-timeline-api/main.go
+++ cmd/ocp-timeline-api/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/ozoncp/ocp-timeline-api/internal/api"
 	desc "github.com/ozoncp/ocp-timeline-api/pkg/ocp-timeline-api"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 )
 
 const (
-	grpcPort = ":1025"
+	grpcPort           = ":1025"
+	grpcServerEndPoint = "localhost:1025"
 )
 
 func run() error {
@@ -29,7 +33,29 @@ func run() error {
 	return nil
 }
 
+func runJSON() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	defer cancel()
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := desc.RegisterOcpTimelineApiHandlerFromEndpoint(ctx, mux, grpcServerEndPoint, opts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = http.ListenAndServe(":8081", mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	go runJSON()
+
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
